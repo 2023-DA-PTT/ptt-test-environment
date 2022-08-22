e705c495d3b9aa14bf697d697b41ddd2e91352ca
naichinger <aichinger.niklas@gmail.com>
2022-08-22 16:27:25 +0000

    add log info

--- src/main/java/com/ptt/boundary/MultipartEndpoint.java
+++ src/main/java/com/ptt/boundary/MultipartEndpoint.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.logging.Logger;
@@ -15,7 +16,8 @@ public class MultipartEndpoint {
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.TEXT_PLAIN)
-    public String testMultipart(String form) {
+    public String testMultipart(HttpHeaders headers, String form) {
+        LOG.info("content-type: " + headers.getHeaderString("content-type"));
         LOG.info("received multipart: " + form);
         return form;
     }
